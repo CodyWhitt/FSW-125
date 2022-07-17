ea92c422af3ac627971b286f71fe1219c3d3307d
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-07-17 11:35:29 -0500

    week 1 submission

---
+++ Week_1/math/app.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+
+console.log('math; ', math.addition(2, 3))
+console.log('math; ', math.subtraction(2, 3))
+console.log('math; ', math.multiply(2, 3))
+console.log('math; ', math.divide(2, 3))
