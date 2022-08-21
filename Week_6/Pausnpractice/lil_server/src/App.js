b5d3b8226d05363a4c7b16b8243ce5c80fe4db74
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-08-21 10:51:16 -0500

    final submission

--- Week_6/Pausnpractice/lil_server/src/App.js
+++ Week_6/Pausnpractice/lil_server/src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
+import {v4 as uuidv4} from "uuid"; //unique ID maker
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [userInput, setUserInput] = useState ([''])
   const [itemsList, setItems] = useState([]);
+  const [itemsDisplay, setItemsDisplay] = useState('')
   const [flag, setFlag] = useState(true);
 
   const handleEvent = event => {
@@ -14,18 +16,26 @@ function App() {
 
   useEffect(() => {
     axios.get('./items')
-      .then(res => console.log(res.data))
+      .then(res => setItems(res.data))
       .catch(err => console.log(err))
   }, [flag]);
 
-  const findItems = (i) => {
-    console.log(i)
+  const findItems = (itemsList) => {
+    const displayItems = itemsList.map((itemsList) => {
+      if (userInput === itemsList.type) {
+        return <div key={uuidv4()}>{itemsList.name}, {itemsList.type}</div>
+      }
+    })
+    setItemsDisplay(displayItems)
   }
 
   return (
     <div className="App">
-      <input onChange={handleEvent} value={userInput}></input>
+      <input onChange={handleEvent} value={userInput} placeholder={'fruit/veggie'}></input>
       <button onClick={() => findItems(itemsList)}>Search</button>
+      <div>
+        {itemsDisplay}
+      </div>
     </div>
   );
 }
